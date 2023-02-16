3c8c2a20f178bff4166a88e73facf5452e74b6f1
Ittideh <2110511101031@live4.utcc.ac.th>
2023-02-16 11:23:16 +0700

    refactor#2 adding paceCal fn

--- common.js
+++ common.js
@@ -9,21 +9,25 @@ function paceValidate(pace){
 		return {"status":0,"message":msg,"hour":0,"min":0};
 	}
 }
+function paceCal(pace,dist){
+	var min = dist * pace;
+		var hour = 0;
+		if(min > 60){
+			hour = Math.floor(min / 60);          
+			min = min % 60;
+		}
+		return{"min":min,"hour":hour};
+}
 
 function calculate(p,d){
 		var msg = "";
 		var pace = parseFloat(p);
 		var dist = parseInt(d);
 	//---------------------------
-	var chk = paceValidate(pace);
-	if (chk != null)return chk;
+		var chk = paceValidate(pace);
+		if (chk != null)return chk;
 	//---------------------------
-		var min = dist * pace;
-		var hour = 0;
-		if(min > 60){
-			hour = Math.floor(min / 60);          
-			min = min % 60;
-		}
+		var pc = paceCal(pace,dist);
 	//---------------------------
 		msg = "successfully calculated!"
 		return {"status":1,"message":msg,"hour":hour,"min":min};
